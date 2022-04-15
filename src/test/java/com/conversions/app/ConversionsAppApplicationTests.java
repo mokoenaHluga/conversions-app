2f9cfebad8cc4e690fadb1c5e2994417e4a93c26
Lehlohonolo Mokoena <lehlohonolomokoena@Lehlohonolos-MacBook-Pro.local>
2022-04-15 11:03:18 +0200

    first commit

---
+++ src/test/java/com/conversions/app/ConversionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
